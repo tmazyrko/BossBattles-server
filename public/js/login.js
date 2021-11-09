1a761d26c78dcb77eca0545acf2e91baeead809e
tmazyrko <tmazyrko@gmail.com>
2021-11-08 22:31:02 -0500

    Add function to store username in a cookie - to be sent to server w/ socket id

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+function saveCookie(name, days) {
+    let expires = "";
+    const value = document.getElementById("username").value;
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/; SameSite=None; Secure";
+}
