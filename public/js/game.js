39f98c990bff5eec260f933c9cc171bd14d12dd9
ek267 <ek267@njit.edu>
2021-11-08 23:02:12 -0500

    obtains user wins and number of characters unlocked

--- public/js/game.js
+++ public/js/game.js
@@ -42,11 +42,11 @@ const init = function() {
     unlockScene = new PIXI.Container();
 
     //testing purposes: victory scene true
-    matchScene.visible = false;
+    matchScene.visible = true;
     lobbyScene.visible = false;
     selectScene.visible = false;
     gameScene.visible = false;
-    victoryScene.visible = true;
+    victoryScene.visible = false;
     unlockScene.visible = false;
 
     app.stage.addChild(matchScene);
@@ -321,6 +321,7 @@ window.onload = function() {
         console.log(socket.id);
         console.log("Username: " + getCookie("username"));
         socket.emit("TX_USERNAME", getCookie("username"));
+        socket.emit("VICTORY_CONTINUE");
     });
 
     socket.on("successful_join", (response) => {
@@ -355,6 +356,11 @@ window.onload = function() {
     function atkSubmit(atk){
         socket.emit("ATK_SUBMIT", atk);
     }
+
+    victoryText.on('pointerdown', function() {
+        //socket.emit("VICTORY_CONTINUE");
+        //console.log("hello");
+    });
 
 };
 
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,7 +10,7 @@ module.exports = Socket = (httpServer) => {
 
     let players = [];
     
-    io.on("connection", (socket) => { 
+    io.on("connection", (socket) => {
         console.log('New user connected. Socket ID: ' + socket.id);
 
         socket.on(SOCKET_ACTIONS.TX_USERNAME, (username) => {
@@ -71,7 +71,23 @@ module.exports = Socket = (httpServer) => {
             // Check for unlockables here
             // If nothing to unlock, send a command to client to go back to main menu scene
             // If there is something to unlock, send a command to client to go to the unlocks scene
-
+            //Check and see if an unlock happens and how many characters they have unlocked
+            //on continue from victory screen
+            console.log("ye")
+            const numCharUnlockQuery = "SELECT NumCharUnlock FROM PlayerInfo WHERE Username = \"" + players[socket.id] + "\";"
+            const numCharUnlock = await rpc(numCharUnlockQuery)
+            const userWinsQuery = "SELECT Wins FROM PlayerInfo WHERE Username = \"" + players[socket.id] + "\";"
+            const userWins = await rpc(userWinsQuery)
+            //console.log(userWins["count(Wins)"] + "user wins");
+            console.log(players[socket.id] + " has " + userWins["count(Wins)"] + " wins")
+            console.log(userWins)
+            if (userWins["Wins"] == 5 && numCharUnlock["NumCharUnlock"] == 0) {
+                //socket.emit("successful_join", {msg: `you have unlocked: `})
+                const addCharUnlockQuery = "UPDATE PlayerInfo SET NumCharUnlock = " + (parseInt(numCharUnlock, 10)+1) + " WHERE Username = \"" + players[socket.id] + "\";"
+                const addCharUnlock = await (rpc(addCharUnlockQuery))
+                console.log(addCharUnlock)
+            //^^ that 4 time, but for 10, 15, 20 and update NumCharUnlock
+            }
             // Two possible options here:
             // Keep both players in a room for now and emit/reply specifically to each client's msgs by saving the socket id of sender and using io.sockets.socket(savedSocketId).emit(...)
             // or
